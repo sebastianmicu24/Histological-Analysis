833fb97cf75a5a1162221f2278c888e91cfd84dc
sebastianmicu24 <sebastianmicu24@gmail.com>
2024-11-04 12:06:59 +0100

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,34 +1,17 @@
-##################### PRELIMINARY STEPS ###################################
+##################### IMPORTS ###################################
 
-# Close all open images
-from ij import WindowManager
-for imp in WindowManager.getImageTitles():
-    WindowManager.getImage(imp).close()
-
-# Clear ROI if it was opened
-from ij.plugin.frame import RoiManager
-rm = RoiManager.getInstance()
-if rm is not None:
-    rm.reset()
-
-from ij import IJ, ImagePlus
+import sys
+import os
+from ij import IJ, ImagePlus, WindowManager
 from ij.process import ImageProcessor
 from ij.plugin.frame import RoiManager
 from ij.io import OpenDialog
 from ij.gui import Roi, ShapeRoi
 
-RM = RoiManager()        # we create an instance of the RoiManager class
-rm = RM.getRoiManager()  # "activate" the RoiManager otherwise it can behave strangely
+##################### FUNCTION DEFINITIONS ###################################
 
-##################### CREATING FUNCTIONS ###################################
-
-
-# Creating the function that takes a binary mask and adds it to the ROI with custom name and colour
-# If invertTrue is = 0 it selects the white part, if it is = 1 it selects the black part
-# Optional fillHolesTrue = 1 adds the holes inside the selection
-# Make sure the number variable is unique for each selection
+# Functions from tissueSelection.py
 def binaryToSelection(number, colour, name, image, fillHolesTrue, invertTrue):
-    
     if invertTrue:
         IJ.run(image, "Invert LUT", "")
     if fillHolesTrue:
@@ -40,33 +23,23 @@ def binaryToSelection(number, colour, name, image, fillHolesTrue, invertTrue):
     rm.runCommand("Rename", name)
     rm.runCommand(image, "Deselect")
 
-
-
-# Function to perform particle selection and add it to ROI Manager
 def particleSelection(minSize, maxSize, image, name, fill, color, invertTrue):
     if invertTrue:
         IJ.run(image, "Invert LUT", "")
     
-    # Store the current number of ROIs before running Analyze Particles
     initial_roi_count = rm.getCount()
-
-    # Run Analyze Particles
-    IJ.run(image, "Analyze Particles...", "size=" + str(minSize) + "-" + str(maxSize) + " pixel show=[Overlay] clear")
+    IJ.run(image, "Analyze Particles...", "size={0}-{1} pixel show=[Overlay] clear".format(minSize, maxSize))
 
     overlay = image.getOverlay()
-
     if overlay is not None and overlay.size() > 0:
         combined_roi = ShapeRoi(overlay.get(0))
         for i in range(1, overlay.size()):
-            roi = overlay.get(i)
-            combined_roi.or(ShapeRoi(roi))
+            combined_roi.or(ShapeRoi(overlay.get(i)))
         
-        # Remove ROIs added by Analyze Particles
         for i in range(rm.getCount() - 1, initial_roi_count - 1, -1):
             rm.select(i)
             rm.runCommand("Delete")
         
-        # Add the combined ROI
         rm.addRoi(combined_roi)
         rm.select(rm.getCount() - 1)
         rm.runCommand("Rename", name)
@@ -75,126 +48,297 @@ def particleSelection(minSize, maxSize, image, name, fill, color, invertTrue):
         if fill.lower() == "true":
             rm.runCommand("Fill")
 
-# New function to create a binary image from an ROI
 def create_binary_image_from_roi(imp, roi, name):
-    # Create a new blank image
     ip = imp.getProcessor().createProcessor(imp.getWidth(), imp.getHeight())
-    ip.setColor(255)  # Set color to white
-    ip.fill()  # Fill the image with white
-    
-    # Draw the ROI in black
     ip.setColor(0)
+    ip.fill()
+    
+    ip.setColor("#ffffff")
     ip.fill(roi)
     
-    # Create a new ImagePlus with the binary image
     binary_imp = ImagePlus(name, ip)
+    
+    IJ.run(binary_imp, "8-bit", "")
+    ip.setColor(255)
+    
+    IJ.setThreshold(binary_imp, 2, 255)
+    IJ.run(binary_imp, "Convert to Mask", "")
+    
     binary_imp.show()
     return binary_imp
 
-# New function to perform binary operations
-def perform_binary_operations(image):
-    IJ.run(image, "Convert to Mask", "");
-    IJ.run(image, "Invert LUT", "")
+def binary_fill_holes(image):
+    IJ.run(image, "8-bit", "")
+    IJ.run(image, "Dilate", "")
+    IJ.run(image, "Dilate", "")
     IJ.run(image, "Dilate", "")
     IJ.run(image, "Fill Holes", "")
     IJ.run(image, "Erode", "")
+    IJ.run(image, "Erode", "")
+    IJ.run(image, "Erode", "")
 
-################ START OF THE CODE ######################
+# Functions from nucleiSelection.py
+def particleSelection_nuclei(minSize, maxSize, image, name, fill, color, invertTrue):
+    if invertTrue:
+        IJ.run(image, "Invert LUT", "")
+    
+    initial_roi_count = rm.getCount()
+    IJ.run(image, "Analyze Particles...", "size={0}-{1} pixel show=[Overlay] add".format(minSize, maxSize))
 
+    final_roi_count = rm.getCount()
+    
+    for i in range(initial_roi_count, final_roi_count):
+        rm.select(i)
+        rm.runCommand("Set Fill Color", color)
+        rm.runCommand("Rename", "{0}_{1}".format(name, i-initial_roi_count+1))
 
-# Open a dialog to select the image file
-od = OpenDialog("Select the image file", None)
-file_path = od.getPath()
+        if fill.lower() == "true":
+            rm.runCommand("Fill")
+    
+    rm.runCommand("Show All")
+    return initial_roi_count, final_roi_count
 
-if file_path is None:
-    print("No file selected. Exiting script.")
-    import sys
-    sys.exit()
+def create_background_points(image, spacing=10):
+    width = image.getWidth()
+    height = image.getHeight()
+    background_points = []
+    
+    for x in range(0, width, spacing):
+        background_points.append((x, 0))
+        background_points.append((x, height - 1))
+    
+    for y in range(spacing, height - spacing, spacing):
+        background_points.append((0, y))
+        background_points.append((width - 1, y))
+    
+    return background_points
 
-# Open the specified image
-imp = IJ.openImage(file_path)
+def create_voronoi_cells(image, initial_count, final_count):
+    points_imp = IJ.createImage("Points", "8-bit black", image.getWidth(), image.getHeight(), 1)
+    points_imp.show()
+    
+    ip = points_imp.getProcessor()
+    ip.setColor(255)
+    
+    for i in range(initial_count, final_count):
+        rm.select(i)
+        roi = rm.getRoi(i)
+        bounds = roi.getBounds()
+        x = bounds.x + bounds.width/2
+        y = bounds.y + bounds.height/2
+        ip.drawDot(int(x), int(y))
+    
+    background_points = create_background_points(image)
+    for x, y in background_points:
+        ip.drawDot(x, y)
+    
+    points_imp.updateAndDraw()
+    
+    IJ.setAutoThreshold(points_imp, "Default dark")
+    IJ.run(points_imp, "Convert to Mask", "")
+    
+    IJ.run(points_imp, "Voronoi", "")
+    
+    IJ.setRawThreshold(points_imp, 1, 255, None)
+    IJ.run(points_imp, "Convert to Mask", "")
+    
+    counter = 0
+    for i in range(initial_count, final_count):
+        rm.select(i)
+        nucleus_roi = rm.getRoi(i)
+        bounds = nucleus_roi.getBounds()
+        x = bounds.x + bounds.width/2
+        y = bounds.y + bounds.height/2
+        
+        IJ.doWand(int(x), int(y))
+        
+        cell_roi = points_imp.getRoi()
+        
+        if cell_roi is not None:
+            rm.addRoi(cell_roi)
+            rm.select(rm.getCount()-1)
+            rm.runCommand("Set Fill Color", "magenta")
+            rm.runCommand("Rename", "Cell_{0}".format(i-initial_count+1))
 
-if imp is None:
-    print("Failed to open image: {}".format(file_path))
-    print("Please check if the file exists and is accessible.")
-    import sys
-    sys.exit()
+##################### MAIN SCRIPT ###################################
 
-imp.setTitle("Copy_1")
-imp.show()
+def main():
+    # Close all open images
+    for imp in WindowManager.getImageTitles():
+        WindowManager.getImage(imp).close()
 
-# Create a duplicate of the original image
-duplicate = imp.duplicate()
-duplicate.setTitle("Copy_2")
-duplicate.show()
+    # Clear ROI Manager if it was opened
+    global rm
+    rm = RoiManager.getInstance()
+    if rm is not None:
+        rm.reset()
 
-# Process the duplicate image
-ImagePlus.setDefault16bitRange(16)
-duplicate.setDisplayRange(60, 215)
-duplicate.updateAndDraw()
-IJ.run(duplicate, "Despeckle", "")
-IJ.run(duplicate, "8-bit", "")
-IJ.setAutoThreshold(duplicate, "Default no-reset")
-IJ.setRawThreshold(duplicate, 220, 255, None)
-IJ.run(duplicate, "Convert to Mask", "")
+    # Initialize ROI Manager
+    RM = RoiManager()
+    rm = RM.getRoiManager()
 
-############## SEPARATING STRUCTURES #################
+    # Open a dialog to select the image file
+    od = OpenDialog("Select the image file", None)
+    file_path = od.getPath()
 
+    if file_path is None:
+        print("No file selected. Exiting script.")
+        return
 
-# Saving the part of the image without tissue as a blue backgroung (it contains the vessels as well)
-binaryToSelection(
-    0,                # number - ROI index (0 for the first ROI)
-    "white",           # colour - Fill color for the ROI
-    "All background", # name - Name for the ROI
-    duplicate,        # image - The image to process
-    False,            # fillHolesTrue - Whether to fill holes (False in this case)
-    False             # invertTrue - Whether to invert the selection (True in this case)
-)
+    # Open the specified image
+    imp = IJ.openImage(file_path)
 
-#Select the tissue with all the vessels inside
+    if imp is None:
+        print("Failed to open image: {0}".format(file_path))
+        print("Please check if the file exists and is accessible.")
+        return
 
-binaryToSelection(
-    1,                # number - ROI index (0 for the first ROI)
-    "white",         # colour - Fill color for the ROI
-    "Tissue",         # name - Name for the ROI
-    duplicate,        # image - The image to process
-    1,                # fillHolesTrue - Whether to fill holes (False in this case)
-    1                 # invertTrue - Whether to invert the selection (True in this case)
-)
-rm.select(1)
+    # Tissue Selection
+    imp.setTitle("Copy_1")
+    imp.show()
 
-# Use the new particleSelection function
-particleSelection(100000, "Infinity", duplicate, "Combined Particles", "false", "magenta", False)
+    duplicate = imp.duplicate()
+    duplicate.setTitle("Copy_2")
+    duplicate.show()
 
-#Separate vessels from the rest of the tissue
-rm.setSelectedIndexes([0,2])
-rm.runCommand(duplicate,"AND")
-rm.addRoi(duplicate.getRoi())
-rm.select(3)
-rm.runCommand("Set Fill Color", "red")
-rm.runCommand("Rename", "Vessels")
+    ImagePlus.setDefault16bitRange(16)
+    duplicate.setDisplayRange(60, 215)
+    duplicate.updateAndDraw()
+    IJ.run(duplicate, "Despeckle", "")
+    IJ.run(duplicate, "8-bit", "")
+    IJ.setAutoThreshold(duplicate, "Default no-reset")
+    IJ.setRawThreshold(duplicate, 215, 255, None)
+    IJ.run(duplicate, "Convert to Mask", "")
 
-particleSelection(1000, 100000, duplicate, "Cleaned Vessels", "false", "brown", False)
+    binaryToSelection(0, "white", "All background", duplicate, False, False)
+    binaryToSelection(1, "white", "Tissue", duplicate, True, True)
+    rm.select(1)
 
-# Create binary image for Cleaned Vessels ROI
-rm.select(rm.getCount() - 1)  # Select the last ROI (Cleaned Vessels)
-cleaned_vessels_roi = rm.getRoi(4)
-binary_cleaned_vessels = create_binary_image_from_roi(imp, cleaned_vessels_roi, "Binary_Cleaned_Vessels")
+    particleSelection(100000, "Infinity", duplicate, "Combined Particles", "false", "magenta", False)
 
-# Perform binary operations on the Cleaned Vessels binary image
-perform_binary_operations(binary_cleaned_vessels)
+    rm.setSelectedIndexes([0,2])
+    rm.runCommand(duplicate, "AND")
+    rm.addRoi(duplicate.getRoi())
+    rm.select(3)
+    rm.runCommand("Set Fill Color", "red")
+    rm.runCommand("Rename", "Vessels")
 
-# Convert the processed binary image back to ROI
-IJ.run(binary_cleaned_vessels, "Create Selection", "")
-processed_roi = binary_cleaned_vessels.getRoi()
+    particleSelection(2000, 1000000, duplicate, "Cleaned Vessels", "false", "black", False)
 
-# Add the processed ROI to the ROI Manager
-rm.addRoi(processed_roi)
-rm.select(rm.getCount() - 1)
-rm.runCommand("Set Fill Color", "yellow")
-rm.runCommand("Rename", "Processed Cleaned Vessels")
+    rm.select(rm.getCount() - 1)
+    cleaned_vessels_roi = rm.getRoi(4)
+    binary_cleaned_vessels = create_binary_image_from_roi(imp, cleaned_vessels_roi, "Binary_Cleaned_Vessels")
 
-# Close the binary image
-binary_cleaned_vessels.close()
+    binary_fill_holes(binary_cleaned_vessels)
 
-print("Script completed successfully. Processed Cleaned Vessels ROI added to ROI Manager.")
+    IJ.run(binary_cleaned_vessels, "Create Selection", "")
+    processed_roi = binary_cleaned_vessels.getRoi()
+
+    rm.addRoi(processed_roi)
+    rm.select(rm.getCount() - 1)
+    rm.runCommand("Set Fill Color", "yellow")
+    rm.runCommand("Rename", "Processed Cleaned Vessels")
+
+    binary_cleaned_vessels.close()
+
+    rm.select(4)
+    rm.runCommand(duplicate,"Delete")
+    rm.select(3)
+    rm.runCommand(duplicate,"Delete")
+    rm.select(1)
+    rm.runCommand(duplicate,"Delete")
+    rm.select(0)
+    rm.runCommand(duplicate,"Delete")
+
+    rm.setSelectedIndexes([0,1])
+    rm.runCommand(imp,"XOR")
+    IJ.run(imp, "Make Inverse", "")
+    new_roi = imp.getRoi()
+    if new_roi is not None:
+        rm.addRoi(new_roi)
+    rm.select(2)
+    IJ.setForegroundColor(255, 255, 255)
+    rm.runCommand(imp,"Fill")
+
+    duplicate.close()
+
+    print("Tissue selection completed successfully.")
+
+    # Deselect everything without closing any page
+    rm.runCommand(imp, "Deselect")
+    imp.killRoi()
+
+    # Nuclei Selection
+    if imp.getType() != ImagePlus.COLOR_RGB:
+        print("Error: Image must be RGB color. Please select an RGB image.")
+        print("Current image type: {0}".format(imp.getType()))
+        return
+
+    imp.setTitle("Original")
+
+    try:
+        print("Attempting to duplicate image...")
+        duplicate = imp.duplicate()
+        print("Image duplicated successfully.")
+    except Exception as e:
+        print("Error occurred while duplicating image:")
+        print(str(e))
+        return
+
+    duplicate.setTitle("Deconvolved")
+    duplicate.show()
+
+    try:
+        IJ.run(duplicate, "Colour Deconvolution", "vectors=[H&E]")
+
+        nuclei_image = WindowManager.getImage("Deconvolved-(Colour_1)")
+        if nuclei_image is None:
+            print("Error: Color deconvolution failed. Could not get nuclei image.")
+            return
+            
+        nuclei_image.setTitle("Nuclei")
+
+        color2_imp = WindowManager.getImage("Deconvolved-(Colour_2)")
+        if color2_imp is not None:
+            color2_imp.close()
+            
+        color3_imp = WindowManager.getImage("Deconvolved-(Colour_3)")
+        if color3_imp is not None:
+            color3_imp.close()
+
+        IJ.run(nuclei_image, "Despeckle", "")
+        IJ.run(nuclei_image, "Gaussian Blur...", "sigma=2")
+        
+        kernel = "-1 -1 -1 -1 9 -1 -1 -1 -1"
+        IJ.run(nuclei_image, "Convolve...", "text1=[" + kernel + "]")
+        
+        IJ.setRawThreshold(nuclei_image, 0, 237, None)
+        IJ.run(nuclei_image, "Convert to Mask", "")
+        
+        IJ.run(nuclei_image, "Erode", "")
+        IJ.run(nuclei_image, "Dilate", "")
+        
+        IJ.run(nuclei_image, "Watershed", "")
+        
+        IJ.run(nuclei_image, "Fill Holes", "")
+        
+        IJ.run(nuclei_image, "Open", "")
+
+        initial_count, final_count = particleSelection_nuclei(50, 500, nuclei_image, "Nucleus", "false", "black", False)
+
+        create_voronoi_cells(nuclei_image, initial_count, final_count)
+
+        save_dir = os.path.dirname(file_path)
+        base_name = os.path.splitext(os.path.basename(file_path))[0]
+        roi_path = os.path.join(save_dir, base_name + "_ROIs.zip")
+        rm.runCommand("Save", roi_path)
+
+        print("Script completed successfully.")
+        print("Nuclei and cell ROIs added to ROI Manager and saved to: " + roi_path)
+        
+    except Exception as e:
+        print("An error occurred during processing:")
+        print(str(e))
+
+if __name__ == '__main__':
+    main()
